17a325bc5eff0580c95f299520cb580788028c85
David <Dudavik356@yandex.ru>
2024-07-13 15:02:33 +0600

    add long latt field in shelter model

--- backend/pet_seeker/shelter/models.py
+++ backend/pet_seeker/shelter/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
-class Shelter(models.Model):
+from gis_app.models import LocationModelMixin
+
+class Shelter(LocationModelMixin):
     user = models.ForeignKey(User, related_name='shelters', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField()
+
+
+    
--- backend/pet_seeker/shelter/tests.py
+++ backend/pet_seeker/shelter/tests.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
+from shelter.models import Shelter
 from django.test import TestCase
 
-# Create your tests here.
+class ShelterTest(TestCase):
+    def test_create_shelter_with_incorrect_geo(self):
+        user = User.objects.create_user(username="+123", password='123')
+        shelter = Shelter.objects.create(user=user, name="ginger shelter", lattitude_longitude='55.102943, ')

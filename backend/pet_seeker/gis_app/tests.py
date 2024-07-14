d9f30fe0f9d7b04f3178ac4cefe57b47ff4c755d
David <Dudavik356@yandex.ru>
2024-07-14 22:24:13 +0600

    add test for gis with shelter

--- backend/pet_seeker/gis_app/tests.py
+++ backend/pet_seeker/gis_app/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from shelter.models import Shelter
 
 from .services import YandexGeocoderCoordinatesToAddressServicer
 
@@ -9,3 +11,13 @@ class CommonGeocoderTest(TestCase):
             YandexGeocoderCoordinatesToAddressServicer.get_address_from_coordinates((55.101344, 73.350488)),
             'Россия, посёлок Омский, Центральная улица, 12Б'
         ) # my home 
+
+
+class ShelterTest(TestCase):
+    def test_create_shelter_with_incorrect_geo(self):
+        user = User.objects.create_user(username="+123", password='123')
+        shelter = Shelter.objects.create(user=user, name="ginger shelter", lattitude_longitude='55.101344, 73.350488')
+        coordinates = YandexGeocoderCoordinatesToAddressServicer.long_latt_field_to_coordinates(shelter.lattitude_longitude)
+        self.assertEqual(coordinates, (55.101344, 73.350488))
+        address = YandexGeocoderCoordinatesToAddressServicer.get_address_from_coordinates(coordinates)
+        self.assertEqual(address, 'Россия, посёлок Омский, Центральная улица, 12Б')
--- backend/pet_seeker/shelter/tests.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from shelter.models import Shelter
-from django.test import TestCase
-
-class ShelterTest(TestCase):
-    def test_create_shelter_with_incorrect_geo(self):
-        user = User.objects.create_user(username="+123", password='123')
-        shelter = Shelter.objects.create(user=user, name="ginger shelter", lattitude_longitude='55.101344, 73.350488') # my home

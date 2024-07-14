dece27937c03cb88ff298be9ee8aa972bfec1417
David <Dudavik356@yandex.ru>
2024-07-14 21:48:56 +0600

    moved LocationModelMixin to gis_app/model_mixins.py

--- backend/pet_seeker/shelter/models.py
+++ backend/pet_seeker/shelter/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from gis_app.models import LocationModelMixin
+from gis_app.model_mixins import LocationModelMixin
 
 class Shelter(LocationModelMixin):
     user = models.ForeignKey(User, related_name='shelters', on_delete=models.CASCADE)

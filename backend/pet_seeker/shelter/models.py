9d76c19acdd15d5fe1e3e3162c6f2028d20245bd
David <Dudavik356@yandex.ru>
2024-07-15 08:55:01 +0600

    add field contacts field

--- backend/pet_seeker/shelter/models.py
+++ backend/pet_seeker/shelter/models.py
@@ -5,3 +5,4 @@ class Shelter(models.Model):
     user = models.ForeignKey(User, related_name='shelters', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
+    contacts = models.CharField(max_length=100)

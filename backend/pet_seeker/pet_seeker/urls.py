5ad8b29873b8e90d1e3ae840e50ca0dbc5724729
David <Dudavik356@yandex.ru>
2024-07-25 09:40:17 +0600

    change urls.py

--- backend/pet_seeker/pet_seeker/urls.py
+++ backend/pet_seeker/pet_seeker/urls.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 

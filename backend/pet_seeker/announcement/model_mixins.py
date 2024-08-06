786b2166fd0cd5566d3941247a3ba77250205924
David <Dudavik356@yandex.ru>
2024-08-06 23:29:57 +0600

    The ad model has been brought to the requirements

--- backend/pet_seeker/announcement/model_mixins.py
+++ backend/pet_seeker/announcement/model_mixins.py
@@ -1,5 +1,7 @@
 
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from gis_app.model_mixins import LocationModelMixin
 
 
@@ -28,7 +30,7 @@ class AnnouncementMixin(LocationModelMixin):
     state = models.CharField(max_length=50, choices=StateChoices.choices, default=StateChoices.active)
     status = models.CharField(max_length=50, choices=StatusChoices.choices)
     breed = models.CharField(max_length=50, blank=True, null=True)
-    age = models.PositiveIntegerField(blank=False, null=False)
+    age = models.PositiveIntegerField(blank=False, null=False, validators=[MinValueValidator(0), MaxValueValidator(50)])
     gender = models.BooleanField() # True - male, False - female
     wool_type = models.CharField(max_length=50, blank=True, null=True)
     color = models.CharField(max_length=50, null=False, blank=False)
@@ -36,10 +38,12 @@ class AnnouncementMixin(LocationModelMixin):
     allergenicity = models.BooleanField(null=True)
     health_issues = models.CharField(max_length=200, blank=True, null=True)
     vaccinations = models.BooleanField(null=True)
-    weigth = models.FloatField(null=False, blank=False)
-    dimmensions = models.PositiveIntegerField(null=False, blank=False)
+    weigth = models.FloatField(null=False, blank=False, validators=[MinValueValidator(0), MaxValueValidator(150)])
+    dimmensions = models.PositiveIntegerField(null=False, blank=False, validators=[MinValueValidator(0), MaxValueValidator(200)])
     temperament = models.CharField(max_length=100, null=True, blank=True)
-    
+
     class Meta:
         abstract = True
         ordering = ['-published_at']
+
+    
--- backend/pet_seeker/shelter_announcement/models.py
+++ backend/pet_seeker/shelter_announcement/models.py
@@ -6,3 +6,16 @@ from announcement.model_mixins import AnnouncementMixin
 
 class ShelterAnnouncement(AnnouncementMixin):
     shelter = models.ForeignKey(Shelter, related_name='announcements', on_delete=models.CASCADE)
+
+    status = models.CharField(
+        max_length=50, choices=AnnouncementMixin.StatusChoices.choices, default=AnnouncementMixin.StatusChoices.looking_home
+    )
+    address = models.CharField(max_length=100, blank=True, null=True)
+    lattitude_longitude = models.CharField(max_length=100, blank=True, null=True)
+    contacts = models.CharField(max_length=100, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.contacts = self.shelter.contacts
+        self.address = self.shelter.address
+        self.status = AnnouncementMixin.StatusChoices.looking_home
+        super().save(*args, **kwargs)

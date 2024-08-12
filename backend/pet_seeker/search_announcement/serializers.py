e43997844a6947c98ee792f3269253542549a0d0
David <Dudavik356@yandex.ru>
2024-08-12 21:16:56 +0600

    changed search announcment serializer

--- backend/pet_seeker/search_announcement/serializers.py
+++ backend/pet_seeker/search_announcement/serializers.py
@@ -8,4 +8,7 @@ class CommonAnnouncementListSerializer(serializers.Serializer):
     color = serializers.CharField()
     user = serializers.CharField(required=False, source='user.user_info.name')
     shelter = serializers.CharField(required=False, source='shelter.name')
+    images = serializers.SerializerMethodField()
 
+    def get_images(self, obj):
+        return [{'id': image.id, 'url': image.image.url} for image in obj.images.all()]

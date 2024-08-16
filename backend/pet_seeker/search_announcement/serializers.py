9ed7a556106ac05e5bb456ef85e83ec448b398f9
Sergey Badrizlov <badrizlovsergey@gmail.com>
2024-08-16 10:56:26 +0600

    added name, date and adress to CommonAnnouncementListSerializer, merged produciton

--- backend/pet_seeker/search_announcement/serializers.py
+++ backend/pet_seeker/search_announcement/serializers.py
@@ -8,4 +8,10 @@ class CommonAnnouncementListSerializer(serializers.Serializer):
     color = serializers.CharField()
     user = serializers.CharField(required=False, source='user.user_info.name')
     shelter = serializers.CharField(required=False, source='shelter.name')
+    images = serializers.SerializerMethodField()
+    name = serializers.CharField()
+    published_at = serializers.CharField()
+    address = serializers.CharField()
 
+    def get_images(self, obj):
+        return [{'id': image.id, 'url': image.image.url} for image in obj.images.all()]

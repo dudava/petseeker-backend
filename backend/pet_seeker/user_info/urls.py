25b158dbcef4e18213861f87d29e2b0db8071323
Sergey Badrizlov <badrizlovsergey@gmail.com>
2024-08-27 11:31:35 +0600

    Delete user route

--- backend/pet_seeker/user_info/urls.py
+++ backend/pet_seeker/user_info/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('me/', views.UserMeView.as_view()),
     path('user_info/', views.UserInfoEditView.as_view()),
     path('', include(router.urls)),
+    path('delete/<int:pk>/', views.UserDeleteView.as_view()),
 ]
